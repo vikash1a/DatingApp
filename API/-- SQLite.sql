5ffd88cc14dbce87a508a41dcc87664a9a01cab9
vikash1a <vikash1a2b3c@gmail.com>
2020-12-26 22:12:33 +0530

    End of section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,10 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Vikash");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Taushiq");
+
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Harshit");
